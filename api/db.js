a7c87c2026e3befabc691bd46e49cbd309fea4ce
vitsmart <vitsmart2@gmail.com>
2022-10-25 18:52:49 +0100

    backend

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"1234",
-    database:"blog"
+    password:"Odogwu635#",
+    database:"appleblog"
 
 })
--- api/index.js
+++ api/index.js
@@ -1,14 +1,43 @@
-const express = require('express')
-
-
-
+import express from 'express'
+import postRoutes from "./route/posts.js"
+import userRoutes from "./route/users.js"
+import authRoutes from "./route/auth.js"
+import cookieParser from 'cookie-parser'
+import multer from 'multer'
  
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+
+
+
+const storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+     cb(null, '..client/public/uploads')
+   },
+   filename: function (req, file, cb) {
+     cb(null, Date.now() + file.originalname)
+   }
+ })
+ 
+ const upload = multer({ storage: storage })
+app.post('/api/upload', upload.single('file'), function (req, res) {
+   const file = req.file
+res.status(200).json(file.filename)
+})
+
+
 
 const port = process.env.PORT || 5000
 
 
-app.use(express.json())
+
+
 //app.get('/' , (req , res)=>{
 
    //res.send('hello from simple server :)')
